52603d155481f19097d19baeca0d4ea53d23ace3
phat181999 <phat181999@gmail.com>
2024-06-28 00:07:02 +0700

    fix bug

--- src/main/java/com/example/demo/security/SecurityConstants.java
+++ src/main/java/com/example/demo/security/SecurityConstants.java
@@ -1,7 +1,7 @@
 package com.example.demo.security;
 
 public class SecurityConstants {
-    public static final String SECRET = "SecretKeyToGenJWTs";
+    public static final String SECRET = "tanphat99";
     public static final long EXPIRATION_TIME = 3_600_000; // 10 days
     public static final String TOKEN_PREFIX = "Bearer ";
     public static final String HEADER_STRING = "Authorization";
