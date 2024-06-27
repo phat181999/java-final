68e3b99fcb1421dd91d5e36d2ba5b5402c5f6366
phat181999 <phat181999@gmail.com>
2024-06-28 00:06:41 +0700

    fix bug

--- src/main/java/com/example/demo/security/WebSecurityConfiguration.java
+++ src/main/java/com/example/demo/security/WebSecurityConfiguration.java
@@ -32,7 +32,6 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .and()
                 .addFilter(new JWTAuthenticationFilter(authenticationManager()))
                 .addFilter(new JWTAuthenticationVerificationFilter(authenticationManager()))
-                // this disables session creation on Spring Security
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
 
